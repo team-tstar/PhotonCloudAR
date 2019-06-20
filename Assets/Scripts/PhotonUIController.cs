203031c96bf7fa78a6efc10256e9e851045378de
Neel <neel.kukreti23@gmail.com>
2019-06-20 17:10:26 +0530

    photon copy of coudAR

---
+++ Assets/Scripts/PhotonUIController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class PhotonUIController : MonoBehaviourPunCallbacks
+{
+    bool isConnecting;
+    string gameVersion = "1";
+    byte maxPlayersPerRoom=4;
+    public GameObject namae;
+    public void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    public void Connect()
+    {
+        // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
+        isConnecting = true;
+
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = this.maxPlayersPerRoom;
+            PhotonNetwork.JoinOrCreateRoom("123",options,TypedLobby.Default);
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = this.gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+    /// <summary>
+    /// Called after the connection to the master is established and authenticated
+    /// </summary>
+    public override void OnConnectedToMaster()
+    {
+        // we don't want to do anything if we are not attempting to join a room. 
+        // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
+        // we don't want to do anything.
+        if (isConnecting)
+        {
+            
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room.\n Calling: PhotonNetwork.JoinRandomRoom(); Operation will fail if no room found");
+            _ShowAndroidToastMessage("connected to server");
+            // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
+            //PhotonNetwork.JoinRoom("test");
+        }
+    }
+
+    /// <summary>
+    /// Called when a JoinRandom() call failed. The parameter provides ErrorCode and message.
+    /// </summary>
+    /// <remarks>
+    /// Most likely all rooms are full or no rooms are available. <br/>
+    /// </remarks>
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        //PhotonNetwork.CreateRoom("test", new RoomOptions { MaxPlayers = this.maxPlayersPerRoom });
+
+    }
+
+
+    /// <summary>
+    /// Called after disconnecting from the Photon server.
+    /// </summary>
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+      
+        Debug.LogError("PUN Basics Tutorial/Launcher:Disconnected");
+
+        // #Critical: we failed to connect or got disconnected. There is not much we can do. Typically, a UI system should be in place to let the user attemp to connect again.
+      
+
+        isConnecting = false;
+        
+        this.gameObject.SetActive(true);
+        //controlPanel.SetActive(true);
+
+    }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Creation");
+        _ShowAndroidToastMessage("created room");
+    }
+    /// <summary>
+    /// Called when entering a room (by creating or joining it). Called on all clients (including the Master Client).
+    /// </summary>
+    /// <remarks>
+    /// This method is commonly used to instantiate player characters.
+    /// If a match has to be started "actively", you can call an [PunRPC](@ref PhotonView.RPC) triggered by a user's button-press or a timer.
+    ///
+    /// When this is called, you can usually already access the existing players in the room via PhotonNetwork.PlayerList.
+    /// Also, all custom properties should be already available as Room.customProperties. Check Room..PlayerCount to find out if
+    /// enough players are in the room to start playing.
+    /// </remarks>
+    public override void OnJoinedRoom()
+    {
+        _ShowAndroidToastMessage("joined");
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.\nFrom here on, your game would be running.");
+        namae.GetComponent<PhotonNameSetter>().SetPlayerName(namae.GetComponent<InputField>().text);
+        // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.AutomaticallySyncScene to sync our instance scene.
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the ToDo");
+
+            // #Critical
+            // Load the Room Level. 
+            //this.gameObject.SetActive(false);
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
---
+++ Assets/SpawnerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnerController : MonoBehaviourPunCallbacks
+{
+[SerializeField] private GameObject[] prefabs;
+private int rand;
+private Touch touch;
+private Vector3 tpose;
+
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        //     Input.touchCount = 0;
+    }
+    public void bttnscript()
+    {
+        PhotonNetwork.Instantiate("AndyGreenDiffuse", tpose, Quaternion.identity,0);
+    }
+}
